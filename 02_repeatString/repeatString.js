fa713eed46253e1d2241ad01c499d88cbcf0bce7
jacob-dt <jdunix10@gmail.com>
2022-07-26 19:08:47 -0400

    fix repeatString files to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, x) {
+  let output = '';
+  for(let i = 0; i < x; i++) {
+    if (x < 0) {
+      output += 'ERROR';
+    }
+    else {
+      output += str;
+    }
+  }
+  return output;
 };
+
+console.log(repeatString('hello' , -1));
 
 // Do not edit below this line
 module.exports = repeatString;

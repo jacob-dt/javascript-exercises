d76add9a17f6e1e8ec1abeb130541e454b1cc7be
jacob-dt <jdunix10@gmail.com>
2022-09-18 16:15:41 -0400

    solve sumAll.js and test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let sum = 0;
+  if (x > 0 && y > 0 && typeof x === 'number' && typeof y === 'number') {
+    if (x < y) {
+      while (x <= y) {
+        sum = sum + x;
+        x++;
+      }
+      return sum;
+    }
+    else {
+      while (x >= y) {
+        sum = sum + y;
+        y++;
+      }
+      return sum; 
+    }
+  }
+  else {
+    return "ERROR";
+  } 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

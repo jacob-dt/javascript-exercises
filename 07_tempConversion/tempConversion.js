68a81241ea9da8de5053ebb824a4f5bee252ae18
jacob-dt <jdunix10@gmail.com>
2022-09-18 17:08:49 -0400

    solve tempConversion.js and run tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  x = (x - 32.0) * 5.0/9.0;
+  let round1 = Math.round(x * 10) / 10;
+  return round1;
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+  x = (x * 9.0/5.0) + 32.0;
+  let round1 = Math.round(x * 10) / 10;
+  return round1;
 };
 
 // Do not edit below this line
